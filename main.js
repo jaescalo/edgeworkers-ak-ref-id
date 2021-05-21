dbf49e0fd1b6595b0ca4da6da4d12fe83b1c6b20
Jaime Escalona <jaescalo@akamai.com>
2021-05-21 15:40:21 -0600

    New use case

---
+++ main.js
@@ -0,0 +1,43 @@
+/*
+(c) Copyright 2020 Akamai Technologies, Inc. Licensed under Apache 2 license.
+Version: 1.1
+Purpose:  Modify an HTML streamed response by replacing a text string across the entire response. The replacement string is stored in NetStorage.
+*/
+
+import { httpRequest } from 'http-request';
+import { createResponse } from 'create-response';
+import { TextEncoderStream, TextDecoderStream } from 'text-encode-transform';
+import { FindAndReplaceStream } from 'find-replace-stream.js';
+import { logger } from 'log';
+
+const htmlEndPoint = '/json-inline-demo/index.html';
+const jsonRefIdData = {
+                      "0":"ERR_NONE",
+                      "18":"ERR_ACCESS_DENIED",
+                      "52":"ERR_INVALID_CLIENT_CERT",
+                      "97":"ERR_CONNECT_TIMEOUT",
+                      };
+
+export async function responseProvider (request) {
+  
+  let pmuserVar = request.getVariable('PMUSER_GRN');
+  logger.log("PMUSER_GRN = %s", pmuserVar);
+
+  // Get text to be searched for and new replacement text from Property Manager variables in the request object.
+  const tosearchfor = "</body>";
+
+  // Text for the replacement
+  const toreplacewith = "\t" + `<data class="json-data" value='` + pmuserVar + jsonRefIdData + "'></data>\n</body>";
+
+  // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
+  const howManyReplacements = 1;
+
+  return httpRequest(htmlEndPoint).then(response => {
+    return createResponse(
+      response.status,
+      response.headers,
+      response.body.pipeThrough(new TextDecoderStream()).pipeThrough(new FindAndReplaceStream(tosearchfor, toreplacewith, howManyReplacements)).pipeThrough(new TextEncoderStream())
+    );
+  });
+}
+
