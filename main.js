38b799cb3d7fcbf89da3569996a2a3ea602354e0
Jaime Escalona <jaescalo@akamai.com>
2021-05-21 17:40:58 -0600

    Return error string

--- main.js
+++ main.js
@@ -14,27 +14,31 @@ const htmlEndPoint = '/json-inline-demo/index.html';
 const jsonRefIdData = {
                       "0":"ERR_NONE",
                       "18":"ERR_ACCESS_DENIED",
+                      "21":"ERR_EDGEWORKER",
                       "52":"ERR_INVALID_CLIENT_CERT",
                       "97":"ERR_CONNECT_TIMEOUT",
                       };
 
 export async function responseProvider (request) {
   
-  let akamiRefId = request.getVariable('PMUSER_GRN');
-  logger.log("PMUSER_GRN = %s", akamiRefId);
+  let akamaiRefId = request.getVariable('PMUSER_GRN');
+  logger.log("PMUSER_GRN = %s", akamaiRefId);
 
-  let akamaiRefIdHead = akamiRefId.split(/\./)[0];
+  let akamaiRefIdHead = akamaiRefId.split(/\./)[0];
   logger.log(akamaiRefIdHead);
 
   for (key in jsonRefIdData) {
-    logger.log(key)
+    if(akamaiRefIdHead === key) {
+      logger.log(jsonRefIdData[key]);
+      break;
+    }    
   }
 
   // Get text to be searched for and new replacement text from Property Manager variables in the request object.
   const tosearchfor = "</body>";
 
   // Text for the replacement
-  const toreplacewith = "\t" + `<data class="json-data" value='` + akamiRefId + jsonRefIdData + "'></data>\n</body>";
+  const toreplacewith = "\t" + `<data class="json-data" value='` + akamaiRefId + jsonRefIdData + "'></data>\n</body>";
 
   // Set to 0 to replace all, otherwise a number larger than 0 to limit replacements
   const howManyReplacements = 1;
